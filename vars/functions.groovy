41b2c7d609cfa9eaf66bf3dbec51569e31cbb5ed
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2024-02-27 00:40:36 +0530

    Create functions.groovy

---
+++ vars/functions.groovy
@@ -0,0 +1,42 @@
+vars
+| --- functions.groovy
+
+def call(String repoUrl){
+  pipeline {
+       agent any
+       tools {
+           maven 'Maven 3.6.3'
+           jdk 'jdk8'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
+
+}
