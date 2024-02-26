e2fbb21a2fcc2439c9c1b9b6c11d516198f02a8e
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2024-02-27 00:50:27 +0530

    Update functions.groovy

--- vars/functions.groovy
+++ vars/functions.groovy
@@ -36,6 +36,12 @@ def call(String repoUrl){
                    sh "mvn package -DskipTests"
                }
            }
+         stage("Build Artifact") {
+               steps {
+              sh "mvn clean package -DskipTests=true"
+              archive 'target/*.jar' //so that they can be downloaded later
+               }
+           }
        }
    }
 
