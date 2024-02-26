7cc9d57b82f561cc14e64cc9d1c5c0aeb42a663a
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2024-02-27 02:25:35 +0530

    Create functions1.groovy

---
+++ vars/functions1.groovy
@@ -0,0 +1,66 @@
+// vars
+// | --- functions.groovy
+
+def call(String repoUrl){
+  pipeline {
+       agent any
+       // tools {
+       //     maven 'Maven 3.6.3'
+       //     jdk 'jdk8'
+       // }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           // stage("Running Testcase") {
+           //    steps {
+           //         sh "mvn test"
+           //     }
+           //   post{
+           //     always{
+           //       junit 'target/surefire-reports/*.xml'
+           //       jacoco execPattern: 'target/jacoco.exec'
+           //     }
+           //   }
+           // }
+ stage("Running Testcase") {
+              steps {
+                   sh '''mvn -B -e org.jacoco:jacoco-maven-plugin:0.8.5:prepare-agent clean install -Dmaven.wagon.http.ssl.insecure=true -f pom.xml -Dmaven.test.skip='true' \
+                    -DjvmArgs="-Xmx1G -XX:PermSize=128m -XX:MaxPermSize=256m" '''
+               }
+           }
+         
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+         stage("Build Artifact") {
+               steps {
+              sh "mvn clean package -DskipTests=true"
+              archive 'target/*.jar' //so that they can be downloaded later
+               }
+           }
+         stage('SonarQube') {
+            steps {
+              sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar "
+            }
+        }
+       }
+   }
+
+}
